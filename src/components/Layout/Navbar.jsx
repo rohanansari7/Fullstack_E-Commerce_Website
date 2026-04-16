4cc093b1301002fabc21607ec95cb74046d29fde
Ayan Ansari <123437439+rohanansari7@users.noreply.github.com>
2026-04-16 13:04:57 +0530

    Delete src directory

--- src/components/Layout/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import { Menu, User, ShoppingCart, Sun, Moon, Search } from "lucide-react";
-import { useTheme } from "../../contexts/ThemeContext";
-import { useDispatch, useSelector } from "react-redux";
-
-const Navbar = () => {
-  return <></>;
-};
-
-export default Navbar;
